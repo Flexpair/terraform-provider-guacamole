da72ff1cf03ec9dc267b815eded448a88f2c3a1a
Flexpair.com <153352854+Flexpair@users.noreply.github.com>
2026-09-05 14:58:44 +0200

    fix: restore shared boolean conversion helper

--- guacamole/structure.go
+++ guacamole/structure.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
